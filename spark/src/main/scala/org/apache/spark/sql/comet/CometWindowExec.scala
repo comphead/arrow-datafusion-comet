c40f8adc4e2084017f3c9a9b7640064b88d9a21a
comphead <comphead@ukr.net>
2026-06-02 18:31:53 -0700

    fix windows tests

--- spark/src/main/scala/org/apache/spark/sql/comet/CometWindowExec.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/CometWindowExec.scala
@@ -20,7 +20,8 @@
 package org.apache.spark.sql.comet
 
 import scala.jdk.CollectionConverters._
-import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, AttributeSet, CumeDist, CurrentRow, DenseRank, Expression, Lag, Lead, Literal, MakeDecimal, NTile, NamedExpression, NthValue, PercentRank, RangeFrame, Rank, RowFrame, RowNumber, SortOrder, SpecialFrameBoundary, SpecifiedWindowFrame, UnboundedFollowing, UnboundedPreceding, WindowExpression}
+
+import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, AttributeSet, CumeDist, CurrentRow, DenseRank, Expression, Lag, Lead, Literal, MakeDecimal, NamedExpression, NthValue, NTile, PercentRank, RangeFrame, Rank, RowFrame, RowNumber, SortOrder, SpecialFrameBoundary, SpecifiedWindowFrame, UnboundedFollowing, UnboundedPreceding, WindowExpression}
 import org.apache.spark.sql.catalyst.expressions.aggregate.{AggregateExpression, Average, Complete, Count, First, Last, Max, Min, Sum}
 import org.apache.spark.sql.catalyst.plans.physical.Partitioning
 import org.apache.spark.sql.execution.SparkPlan
@@ -29,11 +30,12 @@ import org.apache.spark.sql.execution.window.WindowExec
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{DateType, DecimalType, LongType, NumericType}
 import org.apache.spark.sql.types.Decimal
+
 import com.google.common.base.Objects
-import org.apache.comet.CometSparkSessionExtensions.withFallbackReason
+
 import org.apache.comet.{CometConf, ConfigEntry}
-import org.apache.comet.serde.{AggSerde, CometOperatorSerde}
-import org.apache.comet.serde.{AggSerde, CometOperatorSerde, Incompatible, OperatorOuterClass, SupportLevel}
+import org.apache.comet.CometSparkSessionExtensions.withFallbackReason
+import org.apache.comet.serde.{AggSerde, CometOperatorSerde, LiteralOuterClass, OperatorOuterClass}
 import org.apache.comet.serde.OperatorOuterClass.Operator
 import org.apache.comet.serde.QueryPlanSerde.{aggExprToProto, exprToProto, scalarFunctionExprToProto, serializeDataType}
 
@@ -189,10 +191,8 @@ object CometWindowExec extends CometOperatorSerde[WindowExec] {
         case _: CumeDist =>
           (None, scalarFunctionExprToProto("cume_dist"), false)
         case nt: NTile =>
-          // Known correctness bug: Comet's NTILE produces different bucket
-          // assignments than Spark; tracked in #4255. Fall back to Spark.
-          withFallbackReason(windowExpr, "NTILE has a correctness bug in Comet tracked in #4255", nt)
-          (None, None, false)
+          val func = scalarFunctionExprToProto("ntile", exprToProto(nt.buckets, output))
+          (None, func, false)
         case nv: NthValue =>
           val inputExpr = exprToProto(nv.input, output)
 
@@ -277,7 +277,9 @@ object CometWindowExec extends CometOperatorSerde[WindowExec] {
               case i: Integer => i.toLong
               case l: Long => l
               case _ =>
-                withFallbackReason(windowExpr, s"Unsupported ROWS frame lower offset: $e (${e.dataType})")
+                withFallbackReason(
+                  windowExpr,
+                  s"Unsupported ROWS frame lower offset: $e (${e.dataType})")
                 return None
             }
             OperatorOuterClass.LowerWindowFrameBound
@@ -326,7 +328,9 @@ object CometWindowExec extends CometOperatorSerde[WindowExec] {
               case i: Integer => i.toLong
               case l: Long => l
               case _ =>
-                withFallbackReason(windowExpr, s"Unsupported ROWS frame upper offset: $e (${e.dataType})")
+                withFallbackReason(
+                  windowExpr,
+                  s"Unsupported ROWS frame upper offset: $e (${e.dataType})")
                 return None
             }
             OperatorOuterClass.UpperWindowFrameBound
